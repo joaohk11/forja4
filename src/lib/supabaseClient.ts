75a5a9a329fee63c60934f4ae0f7378d31477ef9
joaohk11 <patiosul11@gmail.com>
2026-03-21 21:32:30 -0300

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,7 @@
-import { createClient } from '@supabase/supabaseClient-js';
+// ../lib/supabaseClient.ts
+import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://kafvgcpxlubtpavrrtrs.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_MJ2DG16KmyLO2gbpY5mxeQ_nr85IUA9';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
